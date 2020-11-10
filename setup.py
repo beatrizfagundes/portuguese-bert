0f98ee504d8adaf162be1f84548c893fa4c1d2af
Beatriz Fagundes <beatrizsfslima@gmail.com>
2020-11-09 22:02:04 -0300

    Fix using github dependency on setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import find_packages, setup
 setup(name='portuguese_bert',
       packages=find_packages(),
       install_requires=[
-          'pytorch-transformers==1.1.0', 'seqeval==0.0.12', 'jsonlines==1.2.0',
-          'scikit-learn'
-      ],
-      dependency_links=[
-        'git+https://github.com/kmkurn/pytorch-crf.git@4cd79bc8af55fb0f34a2a39b2e38f0e71c208fd4#egg=pytorch_crf'
+        'pytorch-transformers==1.1.0', 'seqeval==0.0.12', 'jsonlines==1.2.0',
+        'scikit-learn',
+        'pytorch_crf @ git+https://github.com/kmkurn/pytorch-crf.git@4cd79bc8af55fb0f34a2a39b2e38f0e71c208fd4#egg=pytorch_crf'
       ]
 )
